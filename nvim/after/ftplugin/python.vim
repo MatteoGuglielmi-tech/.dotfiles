80fe795d2586b6910032279d20585956d26012f9
Matteo-Tech <matteo.guglilemi.ma.gu@gmail.com>
2022-10-14 18:11:02 +0200

    Fixed folding issues

---
+++ nvim/after/ftplugin/python.vim
@@ -0,0 +1,4 @@
+setlocal noexpandtab
+setlocal smarttab
+setlocal shiftwidth=4
+setlocal tabstop=4
---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,34 @@
+vim.opt.guicursor = ""
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.laststatus = 3
+vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 10
+vim.opt.pumheight = 10
+
+vim.opt.hidden = true
+vim.opt.swapfile = false
+vim.opt.wrap = false
+
+vim.opt.foldmethod = "manual"
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+
+vim.cmd(
+[[
+  au BufWinLeave * silent! mkview!
+  au BufWinEnter * silent! loadview
+  ]]
+)
